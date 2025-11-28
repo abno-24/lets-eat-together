4db94992cd3b57da748f812c14685509328eb3d4
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-11-28 16:58:50 +0530

    feat: add CLOUDINARY_IMG_URL constant and update Header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center">
+    <header className="flex justify-between items-center uppercase">
       <div className="flex flex-col justify-center items-center text-black">
         <span className="text-lg font-medium">Let's eat</span>
         <h2 className="text-xl font-bold -mt-2">Together</h2>
