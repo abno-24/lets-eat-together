d814f3f36a4cdc458e3a1953d4db0e67915abc8b
Abhinav Maurya <abhinavmaurya605@gmail.com>
2025-12-02 20:48:10 +0530

    style: refine Header component styling for consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center uppercase">
-      <div className="flex flex-col justify-center items-center text-black">
+    <header className="flex justify-between items-center">
+      <div className="flex flex-col justify-center items-center text-black ">
         <span className="text-lg font-medium">Let's eat</span>
-        <h2 className="text-xl font-bold -mt-2">Together</h2>
+        <h2 className="uppercase font-bold -mt-2">Together</h2>
       </div>
-      <ul className="flex gap-8">
-        <li className="hover:font-medium cursor-pointer">Home</li>
+      <ul className="flex gap-8 font-medium">
+        <li className="cursor-pointer">Home</li>
         <li>About</li>
         <li>Order</li>
         <li>Contact</li>
